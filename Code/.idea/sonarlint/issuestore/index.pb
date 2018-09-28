e04f93a4878c473d990d37252c8658cac786ec3b
Kevin <kevinnathaniel497@yahoo.com>
2018-09-28 22:30:27 +0700

    Latest 2018, Sept 28, 10:30

---
+++ Code/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,5 @@
+
+8
+Code.iml,f/2/f2ac0cdd82923ea5adee137c1a4220d22ba36492
+I
+src/com/timothy/Main.java,f/a/fa147e34f5fc753bca33cc49cc26a837b2ef9bba
--- Code/src/com/timothy/Main.java
+++ Code/src/com/timothy/Main.java
@@ -8,6 +8,7 @@ public class Main {
     public static void main(String[] args) {
         // write your code here
         Scanner scan = new Scanner(System.in);
+
         int matriksBrs;
         int matriksKol;
         System.out.print("Masukan Jumlah Baris Matriks: ");
@@ -24,12 +25,13 @@ public class Main {
             System.out.print("Masukan Jumlah Kolom Matriks: ");
             matriksKol = scan.nextInt();
         }
-        int[][] matriks = new int[matriksBrs+1][matriksKol+1];
+        double[][] matriks = new double[matriksBrs+1][matriksKol+1];
         inputMatriks(scan, matriks, matriksBrs, matriksKol);
+        gauss(matriks, matriksBrs, matriksKol);
         printMatriks(matriks, matriksBrs, matriksKol);
     }
 
-    public static void inputMatriks(Scanner scan, int[][] matriks, int matriksBrs, int matriksKol) {
+    public static void inputMatriks(Scanner scan, double[][] matriks, int matriksBrs, int matriksKol) {
         System.out.println("Masukan Matriks");
         for (int i = 1; i <= matriksBrs; i++) {
             for (int j = 1; j <= matriksKol; j++) {
@@ -38,7 +40,7 @@ public class Main {
         }
     }
 
-    public static void printMatriks(int[][] matriks, int matriksBrs, int matriksKol){
+    public static void printMatriks(double[][] matriks, int matriksBrs, int matriksKol){
         for (int i = 1; i <= matriksBrs; i++)
         {
             for (int j=1; j <= matriksKol; j++)
@@ -49,8 +51,8 @@ public class Main {
         }
     }
 
-    public static void tukerBrs(int[][] matriks, int matriksKol, int bar1, int bar2){
-        int[] brstemp = new int[matriksKol+1];
+    public static void tukerBrs(double[][] matriks, int matriksKol, int bar1, int bar2){
+        double[] brstemp = new double[matriksKol+1];
         for (int i = 1; i<=matriksKol; i++){
             brstemp[i] = matriks[bar1][i];
             matriks[bar1][i] = matriks[bar2][i];
@@ -58,8 +60,8 @@ public class Main {
         }
     }
 
-    public static void tukerKol(int[][] matriks, int matriksBrs, int kol1, int kol2){
-        int[] koltemp = new int[matriksBrs];
+    public static void tukerKol(double[][] matriks, int matriksBrs, int kol1, int kol2){
+        double[] koltemp = new double[matriksBrs];
         for (int i = 1; i<=matriksBrs; i++){
             koltemp[i] = matriks[i][kol2];
             matriks[i][kol1] = matriks[i][kol2];
@@ -67,10 +69,44 @@ public class Main {
         }
     }
 
-    public static void gauss(){
+    public static void gauss(double[][] matriks, int matriksBrs, int matriksKol) {
+        int baref,kolef;
+        double pembagi;
+        baref = matriksBrs;
+        kolef = matriksKol;
+
+        for(int k=1;k<=baref;k++){
+            //Membagi dengan angka kolom pertama
+            for(int i=k;i<=baref;i++){
+                pembagi = matriks[i][k];
+                for(int j=k;j<=kolef;j++){
+                    matriks[i][j] = (matriks[i][j])/pembagi;
+                }
+            }
+            //Mengurangi dengan kolom atasnya
+            for(int i=k+1;i<=baref;i++){
+                for(int j=k;j<=baref;j++){
+                    matriks[i][j]=matriks[i][j]-matriks[k][j];
+                }
+            }
+        }
+    }
+/* for (i = 1; i < (M).NKolEff; i++) //Per Kolom, biarin paling atas lanjut kebawah
+		{
+			for (j = i + 1; j <= (M).NBrsEff; j++)
+			{
+				temp = MTemp[j][i];
+				for (k = 1; k <= (M).NBrsEff; k++)
+				{
+					MTemp[j][k] = MTemp[j][k] - ((MTemp[i][k] * 1.0) * ((temp * 1.0) / ((MTemp[i][i]) * 1.0)));
+				}
+			}
+		}
+*/
+    public static void gaussjordan(double[][] matriks, int matriksBrs, int matriksKol){
+
 
 
     }
-
 
 }
