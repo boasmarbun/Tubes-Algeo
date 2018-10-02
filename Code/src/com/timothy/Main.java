4061d181a65683ff64ce57a6a3a17c9943b78b37
Kevin <kevinnathaniel497@yahoo.com>
2018-10-02 12:10:31 +0700

    Manthul

--- Code/src/com/timothy/Main.java
+++ Code/src/com/timothy/Main.java
@@ -27,7 +27,7 @@ public class Main {
         }
         double[][] matriks = new double[matriksBrs+1][matriksKol+1];
         inputMatriks(scan, matriks, matriksBrs, matriksKol);
-        gauss(matriks, matriksBrs, matriksKol);
+        gaussjordan(matriks, matriksBrs, matriksKol);
         splgauss(matriks, matriksKol, matriksBrs);
         printMatriks(matriks, matriksBrs, matriksKol);
     }
@@ -73,24 +73,28 @@ public class Main {
     public static void gauss(double[][] matriks, int matriksBrs, int matriksKol) {
         double temp=0;
         boolean tuker,isfound;
-
-        for(int i=1;i<=matriksKol;i++){
+        int brs;
+        brs = 1;
+        for(int i=1;i<=(matriksKol - 1);i++){
             // swap
             int l = i;
             tuker = false;
-            while ((l <= matriksBrs) && !(tuker)){
+            while ((l <= matriksBrs) && !(tuker)) {
                 if (matriks[l][i] != 0){
                     tukerBrs(matriks, matriksKol, i, l);
                     tuker = true;
                 }
                 l++;
             }
-            //Gauss
-            for(int j=i+1;j<=matriksBrs;j++){
-                temp=matriks[j][i];
-                for(int k=1;k<=matriksKol;k++){
-                    matriks[j][k] = matriks[j][k] - ((matriks[i][k]) * ((temp) / (matriks[i][i])));
+            if (tuker) {
+                //Gauss
+                for (int j = brs + 1; j <= matriksBrs; j++) {
+                    temp = matriks[j][i];
+                    for (int k = 1; k <= matriksKol; k++) {
+                        matriks[j][k] = matriks[j][k] - ((matriks[brs][k]) * ((temp) / (matriks[brs][i])));
+                    }
                 }
+                brs++;
             }
         }
         //Bagi supaya diagonal utama jadi 1
@@ -103,7 +107,7 @@ public class Main {
                         temp = matriks[i][j];
                     }
                 }
-                if(isfound){
+                if(isfound) {
                     matriks[i][j] = matriks[i][j] / temp;
                 }
             }
@@ -111,16 +115,19 @@ public class Main {
     }
 
     public static void gaussjordan(double[][] matriks, int matriksBrs, int matriksKol){
-        double temp;
+        int index1; //Mencari index pertama ketemu 1
+        double temp; //Menyimpan nilai perkalian
+        boolean found1; //Apakah ada angka 1
 
         gauss(matriks, matriksBrs, matriksKol);
-        //atas segitiga atas jadi 0
-        for(int i=(matriksKol - 1);i>=1;i--){
-            //Gauss
-            for(int j=matriksBrs;j>=i+1;j--){
-                temp=matriks[j][i];
-                for(int k=1;k<=matriksKol;k++){
-                    matriks[j][k] = matriks[j][k] - ((matriks[i][k]) * ((temp) / (matriks[i][i])));
+
+        for(int j=1; j<=matriksKol-1; j++){
+            for(int i=1; i<=matriksBrs; i++){
+                if(i!=j){
+                    temp=matriks[i][j]/matriks[j][j];
+                    for(int k=1; k<=matriksKol; k++){
+                        matriks[i][k]=matriks[i][k]-temp*matriks[j][k];
+                    }
                 }
             }
         }
@@ -130,14 +137,16 @@ public class Main {
 
     public static void splgauss(double[][] matriks,int matriksKol,int matriksBrs){
         //Matriks yang menjadi input adalah matriks augmented
-        double[] variabel = new double[matriksKol+1]; //Menyimpan nilai variabel
+        double[][] variabel = new double[matriksKol+1][2]; //Menyimpan nilai variabel
         double temp; //Menyimpan hasil penjumlahan
         int j,k,l; //Mencari angka yang bukan 0 dalam satu baris
         int count0; //index angka bukan 0 pertama
+        int brs;
 
         //Inisialisasi array spl
         for(int i=1;i<=matriksKol;i++) {
-            variabel[i] = 0;
+            variabel[i][0] = 0; //0 jika parametrik, 1 jika non-parametriks
+            variabel[i][1] = 0; //Value
         }
 
         //Penyelesaian
@@ -147,7 +156,7 @@ public class Main {
 
             //Mencari angka diagonal utama
             k = 1;
-            while ((count0 == 0) && (k<=matriksKol)){
+            while ((count0 == 0) && (k<=matriksKol)) {
                 if ((matriks[i][k]) != 0) {
                     count0++;
                 }
@@ -155,21 +164,49 @@ public class Main {
             }
             k--;
             if((count0>0) && (k != matriksKol)) { //Jika terdapatn nilai diagonal utama
-                j = matriksKol-1;
+                variabel[k][0] = 1;
+                variabel[k][1] = matriks[i][matriksKol];
+                j = matriksKol - 1;
                 while (j > k) {
-                    //if nonparametrik
-                        temp += variabel[j] * matriks[i][j];
+                    if (variabel[j][0] == 1) {
+                        variabel[k][1] -= variabel[j][1] * matriks[i][j];
+                    }
                     j--;
                 }
-                variabel[k] = matriks[i][matriksKol] - temp; //Simpan nilai variabel yang benar
+
+            }
+        }
+        brs = 0;
+        for(int i=1;i<=matriksKol-1;i++) {
+            if (variabel[i][0] == 0){
+                System.out.println("x" + i + "=" + "x" + i);
             }
             else {
-                //variabel[k] = 0; //parametrik
+                System.out.print("x" + i + "=" + variabel[i][1]);
+                brs++;
+                for (int m=1; m<=matriksKol-1; m++) {
+                    if (variabel[m][0] == 0) {
+                        if (matriks[brs][m] > 0) {
+                            System.out.print("-" + matriks[brs][m] + "x" + m);
+                        }
+                        else if (matriks[brs][m] < 0) {
+                            System.out.print(matriks[brs][m] + "x" + m);
+                        }
+                    }
+                }
+                System.out.println();
             }
         }
 
-        for(int i=1 ;i<=matriksKol-1;i++){
-            System.out.println(variabel[i]);
-        }
+//            else { //variabel[j][0] == 0
+//
+//            }
+
+//            if (variabel[k][1] > 0) {
+//                System.out.println("+" + variabel[k][1]);
+//            }
+//            else if (variabel[k][1] < 0) {
+//                System.out.println(variabel[k][1]);
+//            }
     }
 }
